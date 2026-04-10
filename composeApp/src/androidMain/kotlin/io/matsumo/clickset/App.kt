7711eb6aaeee9a4f1b3d22ce8a1e0eca03e8fc3c
matdumo-and <yh134.toisanda@gmail.com>
2026-04-11 07:34:59 +0900

    Fix missing @Composable import in App.kt
    Signed-off-by: matdumo-and <yh134.toisanda@gmail.com>

--- composeApp/src/androidMain/kotlin/io/matsumo/clickset/App.kt
+++ composeApp/src/androidMain/kotlin/io/matsumo/clickset/App.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
